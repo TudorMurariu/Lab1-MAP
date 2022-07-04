9d56a3fa4d3adacd7af34f11624e9e1dc80de028
Tudor <tudorcristianmurariu@yahoo.com>
2022-07-04 20:43:22 +0300

    verificare operatie

--- l1.java
+++ l1.java
@@ -2,7 +2,27 @@ public class l1
 {
     public static void main(String[] args)
     {
-        System.out.println(args[0] + args[1]);
+        boolean ok = true;
+        for(int i=0;i<args.length;i+=2)
+        {
+            
+        }
+
+
+        for(int i=1;i<args.length;i+=2)
+            if(args[i] != "+" && args[i] != "-" && args[i] != "*" && args[i] != "/")
+                {
+                    ok = false;
+                    break;
+                }
+        
+        if(!ok)
+        {
+            System.out.println("Stringul oferit la tastatura nu este o operatie aritmetica!!!");
+            return;
+        }
+        // else    
+        
     }
 
 }
